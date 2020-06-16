0353772dce534019be7798535078f63c334eaeb3
Gabriel de Mello e Silva Carneiro <35747250+gabrieldemello@users.noreply.github.com>
2020-06-16 18:56:43 -0300

    Initial Commit

---
+++ ProvaWeb2/ProvaWeb2/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProvaWeb2.Models
+{
+    public class Context: DbContext
+    {
+        public DbSet<Pessoa> pessoas { get; set; }
+    }
+}
---
+++ ProvaWeb2/ProvaWeb2/Models/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProvaWeb2.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public String Nome { get; set; }
+        public String Sexo { get; set; }
+        public string Endereco { get; set; }
+        public int DataNasc { get; set; }
+        public int CarteraTrab { get; set; }
+        public int Tel { get; set; }
+        public int Cel { get; set; }
+        public string Email { get; set; }
+    }
+}
